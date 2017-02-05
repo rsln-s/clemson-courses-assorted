37691d78882b8e1e87cd8ffff5f9a22e12ef95e7
Ruslan Shaydulin <ruslan@l2sh.ru>
2017-02-05 14:56:40 -0500

    initial commit. Added boxplot plotter

---
+++ 865/hw1pr2-4-boxplot.py
@@ -0,0 +1,31 @@
+## numpy is used for creating fake data
+import numpy as np 
+import matplotlib as mpl 
+import statistics as st
+
+## agg backend is used to create plot as a .png file
+mpl.use('agg')
+
+import matplotlib.pyplot as plt 
+
+age = [23,23,27,27,39,41,47,49,50,52,54,54,56,57,58,58,60,61]
+fat = [9.5,26.5,7.8,17.8,31.4,25.9,27.4,27.2,31.2,34.6,42.5,28.8,33.4,30.2,34.1,30.2,34.1,32.9,41.2,35.7]
+
+print("Age: mean", st.mean(age), "median: ", st.median(age), "stdev: ", st.stdev(age))
+print("Fat: mean", st.mean(fat), "median: ", st.median(fat), "stdev: ", st.stdev(fat)) 
+
+data_to_plot = [np.array(age), np.array(fat)]
+
+# Create a figure instance
+fig = plt.figure(1, figsize=(9, 6))
+
+# Create an axes instance
+ax = fig.add_subplot(111)
+
+# Create the boxplot
+bp = ax.boxplot(data_to_plot)
+
+ax.set_xticklabels(['Age', 'Fat'])
+
+# Save the figure
+fig.savefig('fig2.png', bbox_inches='tight')
